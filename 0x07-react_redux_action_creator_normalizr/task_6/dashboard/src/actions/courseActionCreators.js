5ba57cd0616651f17d6c780981d88548f9a3c6ab
abdelalibakka <pakkatatata@gmail.com>
2024-06-08 00:32:18 +0000

    update

---
+++ 0x07-react_redux_action_creator_normalizr/task_6/dashboard/src/actions/courseActionCreators.js
@@ -0,0 +1,9 @@
+import { SELECT_COURSE, UNSELECT_COURSE } from "./courseActionTypes";
+
+export const selectCourse = (index) => ({type: SELECT_COURSE, index: index});
+
+export const boundSelectCourse = (index) => dispatch(selectCourse(index));
+
+export const unSelectCourse = (index) => ({type: UNSELECT_COURSE, index: index});
+
+export const boundUnSelectCourse = (index) => dispatch(unSelectCourse(index));
