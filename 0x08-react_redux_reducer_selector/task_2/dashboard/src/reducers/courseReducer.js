9f986dc73a2cc6c77a29e95ce5ba070c44d5f4ce
abdelalibakka <pakkatatata@gmail.com>
2024-06-09 21:02:15 +0000

    updat

---
+++ 0x08-react_redux_reducer_selector/task_2/dashboard/src/reducers/courseReducer.js
@@ -0,0 +1,46 @@
+i²mport {
+    FETCH_COURSE_SUCCESS,
+    SELECT_COURSE,
+    UNSELECT_COURSE,
+  } from '../actions/courseActionTypes';
+  
+  const courseReducer = (state = [], action) => {
+    switch (action.type) {
+      case FETCH_COURSE_SUCCESS:
+        return action.data.map((course) => {
+          return {
+            ...course,
+            isSelected: false,
+          };
+        });
+  
+      case SELECT_COURSE:
+        return state.map((course) => {
+          const current = {
+            ...course
+          };
+          if (current.id === action.index) {
+            current.isSelected = true;
+          }
+          return current;
+        });
+  
+      case UNSELECT_COURSE:
+        return state.map((course) => {
+          const current = {
+            ...course,
+          };
+          if (current.id === action.index) {
+            current.isSelected = false;
+          }
+          return current;
+        });
+  
+      default:
+        break;
+    }
+    return state;
+  };
+  
+  export default courseReducer;
+  
